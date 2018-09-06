ae83034c82523d962bc097fde80a7483746dafe5
karolina oska <karolina.oska@netguru.pl>
2018-09-06 20:33:07 +0200

    Add tasks helper and apply fixes to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :provide_task, only: [:edit, :update, :destroy, :done, :undone]
-
+  before_action :authenticate_user!
+  
   def index
     @tasks = current_user.tasks
   end
@@ -15,15 +16,15 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: 'Task was successfully created'
+      redirect_to root_path, notice: 'Task was successfully created'
     else
       render :new
     end
   end
 
   def update
-    if @task.save!
-      redirect_to tasks_path
+    if @task.update(task_params)
+      redirect_to root_path
     else
       render :edit
     end
@@ -31,19 +32,19 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_path, notice: 'Task was successfully deleted'
+    redirect_to root_path, notice: 'Task was successfully deleted'
   end
 
   def done
     @task.completed = true
     @task.save!
-    redirect_to tasks_path
+    redirect_to root_path
   end
 
   def undone
     @task.completed = false
     @task.save!
-    redirect_to tasks_path
+    redirect_to root_path
   end
 
   private
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,7 @@
 module TasksHelper
   def done(task)
     if task.completed
-      link_to 'Already completed!', undone_task_path(task), class: 'btn btn-success'
+      link_to 'Restore', undone_task_path(task), class: 'btn btn-success'
     else
       link_to 'Done!', done_task_path(task), class: 'btn btn-danger'
     end
