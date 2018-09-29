64fdee06d9ca1f8a156b5ba6f7f4fa3240725487
karolina oska <karolina.oska@netguru.pl>
2018-09-29 21:36:03 +0200

    Add additional attributes to user sign up and update views

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,19 @@
 class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
   storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  process resize_and_pad: [100, 100]
+
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
   end
 end
