84be0b5ea0718c0ddb4aa9df993fa1051f316fb8
karolina oska <karolina.oska@netguru.pl>
2018-09-29 16:22:54 +0200

    Create user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'tasks/:id/done', to: 'tasks#done', as: 'done_task'
   get 'tasks/:id/undone', to: 'tasks#undone', as: 'undone_task'
   devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
+  get 'users/:id', to: 'users#show'
 end
