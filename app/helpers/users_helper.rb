0f2fb0267c88bbe75e17cf165e5e7a5aaf9fce49
karolina oska <karolina.oska@netguru.pl>
2018-09-30 09:26:31 +0200

    Add user first and last name to navbar

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,6 @@
+module UsersHelper
+  def full_name
+    return current_user.email unless current_user.first_name? && current_user.last_name?
+    "#{current_user.first_name} #{current_user.last_name}"     
+  end
+end
