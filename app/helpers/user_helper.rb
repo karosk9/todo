b1d46b8873eeb5b5f885b27a1be13ae762e779f8
karolina oska <karolina.oska@netguru.pl>
2018-09-30 10:49:54 +0200

    Rename helpers

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,6 @@
-module UsersHelper
+module UserHelper
   def full_name
     return current_user.email unless current_user.first_name? && current_user.last_name?
-    "#{current_user.first_name} #{current_user.last_name}"     
+    "#{current_user.first_name} #{current_user.last_name}"
   end
 end
