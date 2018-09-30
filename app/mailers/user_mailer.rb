811b2dc581937990052919b054b01b208134c798
karolina oska <karolina.oska@netguru.pl>
2018-09-30 11:42:37 +0200

    Refactor tasks searching with creating scope

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,9 @@
 class UserMailer < ApplicationMailer
+  include UserHelper
   def daily_summary(user_id)
     @user = User.find(user_id)
+    @user_tasks = @user.tasks
+    @tasks_done = @user_tasks.done_yesterday
     mail(
       to: @user.email,
       subject: 'Daily summary'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  scope :done_yesterday, -> { where("Date(finished_at) = ?", 'yesterday') }
 end
