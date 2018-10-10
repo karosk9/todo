64e81e5ce2ceb38b8ce6c406acc64e1f3cf7dc60
karolina oska <karolina.oska@netguru.pl>
2018-10-10 22:13:00 +0200

    Remove task title validation spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,21 +4,6 @@ describe 'Task', type: :model do
   let(:user) { create(:user) }
   let!(:task) { create(:task, user: user) }
 
-  describe 'validations' do
-    it 'is not valid without title' do
-      task = Task.new
-      expect(task).to_not be_valid
-    end
-
-    context 'is valid' do
-      let(:user) { create(:user) }
-      let!(:task) { create(:task, user: user) }
-
-      it { expect(task).to be_valid }
-      it { expect(task).to validate_presence_of(:title) }
-    end
-  end
-
   describe 'associations' do
     it { expect(task).to belong_to(:user) }
   end
