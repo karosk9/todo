509a530268b3c19e651206565fd8ab66f7e725e5
karolina oska <karolina.oska@netguru.pl>
2018-10-28 18:33:39 +0100

    Create show user page

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,8 +1,20 @@
 class UserDecorator < Draper::Decorator
+  include Draper::LazyHelpers
+
   delegate_all
 
   def full_name
     return 'Incognito user' unless first_name? || last_name?
     "#{first_name} #{last_name}"
+  end
+
+  def profile_image_small
+    return '' unless avatar.present?
+    image_tag(avatar, size: 50)
+  end
+
+  def profile_image
+    return '' unless avatar.present?
+    image_tag(avatar, size: 300)
   end
 end
