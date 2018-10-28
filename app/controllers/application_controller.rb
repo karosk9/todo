555b9f71d571e115724a4a79c7e1a1dada29caae
karolina oska <karolina.oska@netguru.pl>
2018-10-28 18:30:08 +0100

    Create user policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception, prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :pundishing_user
 
   protected
+
+  def pundishing_user
+    flash[:error] = 'You have no permission to this action!'
+    redirect_back(fallback_location: root_path)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar avatar_cache remove_avatar])
