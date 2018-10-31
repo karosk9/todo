f07082f561d64baf043e7fab225f2de8da56472a
karolina oska <karolina.oska@netguru.pl>
2018-10-31 22:34:36 +0100

    Set creator user as default assignee

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,16 @@
 class Task < ApplicationRecord
+  before_validation :set_assignee
+
   belongs_to :user
   belongs_to :assignee, class_name: :User, foreign_key: :assignee_id
 
   paginates_per 20
 
   scope :done_yesterday, -> { where('Date(finished_at) = ?', 'yesterday') }
+
+  private
+
+  def set_assignee
+    self.assignee = self.user unless assignee.present?
+  end
 end
