ae43d0b8f400bd5fa550e0a4632c4548e64a5b0c
karolina oska <karolina.oska@netguru.pl>
2018-10-31 22:37:21 +0100

    Create task policy

--- app/decorators/task_decorator.rb
+++ app/decorators/task_decorator.rb
@@ -3,10 +3,12 @@ class TaskDecorator < Draper::Decorator
   delegate_all
 
   def check_if_done
-    if completed?
-      link_to 'Restore', undone_task_path(object), class: 'btn btn-success restore'
-    else
-      link_to 'Done!', done_task_path(object), class: 'btn btn-danger done'
+    if policy(task).manageable?
+      if completed?
+        link_to 'Restore', undone_task_path(object), class: 'btn btn-success restore'
+      else
+        link_to 'Done!', done_task_path(object), class: 'btn btn-danger done'
+      end
     end
   end
 
@@ -27,5 +29,9 @@ class TaskDecorator < Draper::Decorator
 
   def decorated_title
     title.blank? ? 'No title' : title
+  end
+
+  def actions
+    render 'actions', model: task if policy(task).actionable?
   end
 end
